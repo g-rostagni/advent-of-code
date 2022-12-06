c877788ddce1fd71866ed808b391c9600f479826
Guillaume Rostagni <g.rostagni@outlook.com>
2022-12-06 16:22:07 +0000

    Rewrote 2022 day 6 to test the length of a set instead of checking through a string

--- 2022/6.py
+++ 2022/6.py
@@ -1,19 +1,11 @@
 # define a function that returns the marker when the first word with non-repeating characters of length l is found
 def find_marker(data,l):
-	# defing a function that tests whether a string contains repeating characters
-	def check_repeat(s):
-		for char in s:
-			if s.count(char) > 1:
-				return True
-		return False
-	
-	word = data[:l]					# define the starting word of length l
-	for i in range(l,len(data)):			
-		if check_repeat(word):
-			word = word[1:] + data[i]	# shift the word by one character to the right if repeating characters are found
-		else:
+	for i in range(l,len(data)):		
+		# convert the last l characters from the i-th position from the buffer into a set
+		# if the length of the set is exactly l then there are no repeated characters	
+		if len(set(data[i-l:i])) == l:
 			return i
-
+	return -1
 
 with open("6-data","r") as f:
 	data = f.read()
