5130a59fb3048d04cb3d0cd70076bb6dfeeb7bf2
g-rostagni <g.rostagni@outlook.com>
2022-12-11 14:00:50 +0000

    Fixed indents on 2022 day 11 again

--- 2022/11.py
+++ 2022/11.py
@@ -10,14 +10,14 @@ class Monkey:
 	
 	# define what happens when the monkey throws an item
 	def throw(self,item):
-		self.handled += 1													# add 1 to the number of handled items
-		worry_level = eval(self.op.split("=")[-1].replace("old",str(item))) # apply the operation to the worry level (by evaluating the string expression)
-		# worry_level = int(worry_level/3)									# divide the worry level by 3, (un)comment this for part 1 (2)
-		worry_level %= mod_prod												# mod the worry level by a global factor to keep it in check
-		if not worry_level % self.test[0]:									# if the test is satisfied:
-			return self.test[1],worry_level									# return the monkey it is throwing to and the item
+		self.handled += 1							# add 1 to the number of handled items
+		worry_level = eval(self.op.split("=")[-1].replace("old",str(item)))	# apply the operation to the worry level (by evaluating the string expression)
+		# worry_level = int(worry_level/3)					# divide the worry level by 3, (un)comment this for part 1 (2)
+		worry_level %= mod_prod							# mod the worry level by a global factor to keep it in check
+		if not worry_level % self.test[0]:					# if the test is satisfied:
+			return self.test[1],worry_level					# return the monkey it is throwing to and the item
 		else:
-			return self.test[2],worry_level									# return the other monkey it can throw to if not 
+			return self.test[2],worry_level					# return the other monkey it can throw to if not 
 		
 
 with open("11-data","r") as f:
@@ -28,7 +28,7 @@ monkeys = []
 mod_prod = 1
 for monkey in monkeys_text:
 	items = monkey[1].split(":")[-1].split(",")	# the items each monkey is initially carrying
-	op = monkey[2].split(":")[-1]				# the operation each monkey applies on the worry level
+	op = monkey[2].split(":")[-1]			# the operation each monkey applies on the worry level
 	test = []
 	test.append(int(monkey[3].split(" ")[-1]))	# the test each monkey does before throwing
 	test.append(int(monkey[4].split(" ")[-1]))	# the destination if the test is satisfied
@@ -42,11 +42,11 @@ for monkey in monkeys_text:
 # let the monkeys throw stuff around
 rounds = 10000
 for i in range(rounds):
-	for m in range(len(monkeys)):					# loop over each monkey
-		for item in monkeys[m].items:				# loop over each item held by a monkey
+	for m in range(len(monkeys)):				# loop over each monkey
+		for item in monkeys[m].items:			# loop over each item held by a monkey
 			dest,thrown = monkeys[m].throw(item)	# the monkey throws an item to another monkey
-			monkeys[dest].items.append(thrown)		# the destination monkey receives it
-		monkeys[m].items = []						# the monkey loses all its items after throwing them
+			monkeys[dest].items.append(thrown)	# the destination monkey receives it
+		monkeys[m].items = []				# the monkey loses all its items after throwing them
 		
 # determine the number of items handled by each monkey and print the monkey business number
 handled_items = sorted([monkey.handled for monkey in monkeys])
