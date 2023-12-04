bc9735e75a5e2710d497fc7d74ffa4b5dbbb260e
Guillaume Rostagni <g.rostagni@outlook.com>
2023-12-04 12:25:25 +0000

    Added 2023 day 4

---
+++ 2023/4.jl
@@ -0,0 +1,33 @@
+# a function to read the number of winning numbers for each card from the data
+function number_of_wins(f)
+	nwins = Vector{Int64}()							# define a vector that will contain the numbers of winning nos
+	for line in eachline(f)							# loop over each card
+		card = split(line, [':','|'])
+		winning_nums = split(card[2], r" +")				# retrieve the winning numbers
+		my_nums = split(card[3], r" +")					# retrieve our numbers
+		num = count(n->(!isempty(n) && n in winning_nums), my_nums)	# count how many of our numbers are also winning numbers
+		push!(nwins, num)						# add that number to the vector
+	end
+	return nwins
+end
+
+# create an array containing the number of winning numbers we have in each card
+const nwins::Vector{Int64} = number_of_wins("4-data")
+
+score::Int64 = 0			# define the score (part 1)
+ncards = ones(Int64, length(nwins))	# create an array with the number of each card that we have (part 2)
+
+for i in 1:length(nwins)			# loop over all the cards
+	wins = nwins[i]				# get the number of winning numbers we got on that card
+	if wins == 0				# if there are none do nothing
+		continue
+	end
+	
+	global score += 2^(wins - 1)		# add the score of the card to the total
+	for j in 1:wins				
+		global ncards[i+j] += ncards[i]	# add the number of copies that we won to the following cards
+	end
+end
+
+println(score)
+println(sum(ncards))
